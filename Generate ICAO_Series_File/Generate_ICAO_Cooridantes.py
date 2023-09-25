dd50eee9114d4b7dc7f55b752ec8e904d8b0b69c
Aaron_ZSAM <CaptainAaron101@163.com>
2023-09-25 14:17:27 +0800

    坐标转换先开个坑

--- Generate ICAO_Series_File/Generate_ICAO_Cooridantes.py
+++ Generate ICAO_Series_File/Generate_ICAO_Cooridantes.py
@@ -22,3 +22,39 @@ ADdata = cursor.fetchall()
 conn.close
 #endregion
 
+#region 导出结果
+ADNewdata=""
+for i in ADdata:
+    ADNewdata += f"\n{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}"
+#endregion
+
+#region 删除原生ICAO_Airports.txt 中 的国内机场
+# 列出要删除的机场ICAO前两个字符
+ZBBB_FIR_CODE = ["ZB", "ZG", "ZH", "ZJ", "ZL", "ZP", "ZS", "ZU", "ZW", "ZY"]
+
+# 打开文件以读取和写入
+with open("ICAO_Airports.txt", "r+", errors='ignore') as file:
+    lines = file.readlines()
+    file.seek(0)
+    for line in lines:
+        if not any(line.startswith(code) for code in ZBBB_FIR_CODE):
+            file.write(line)
+    file.truncate()
+#endregion
+
+#region 删除带有分号的行
+# 打开ICAO_Airports.txt文件
+with open('ICAO_Airports.txt', 'r') as file:
+    lines = file.readlines()
+
+# 使用列表推导式筛选出不以英文分号开头的行
+filtered_lines = (line for line in lines if not line.startswith(';'))
+
+# 打开同一文件以写入筛选后的内容
+with open('ICAO_Airports.txt', 'w') as file:
+    file.writelines(filtered_lines)
+#endregion
+
+#region 坐标转换
+
+#endregion
