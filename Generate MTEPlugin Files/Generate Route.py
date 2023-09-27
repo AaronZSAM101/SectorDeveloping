1caef7a03ff28c5676d70353f30ce068e2c8834e
Aaron_ZSAM <CaptainAaron101@163.com>
2023-09-27 16:31:49 +0800

    完成数据库读写部分

--- Generate MTEPlugin Files/Generate Route.py
+++ Generate MTEPlugin Files/Generate Route.py
@@ -1,5 +1,7 @@
 import sqlite3
+import re
 import time
+#region 读取数据
 ##读取数据库
 AIRAC_CYCLE = input('当前周期号:')
 DATABASEadress = input(f'{AIRAC_CYCLE}期数据库地址:')
@@ -7,84 +9,33 @@ DATABASEadress = input(f'{AIRAC_CYCLE}期数据库地址:')
 start_time = time.time()
 conn = sqlite3.connect(DATABASEadress)
 cursor = conn.cursor()
-#region 旧方法
-# cursor.execute=('''SELECT
-# 	name,
-# 	round( MinSafeAltitude * 3.281 ) AS 'MinSafeAltitude',
-# 	"RESTRICT",
-# 	TRANS_ALT 
-# FROM
-# 	FLIGHT_AIRLINE 
-# WHERE
-# 	NOT (
-# 		StartAirportID = 'AGAVO' 
-# 		OR StartAirportID = 'APITO' 
-# 		OR StartAirportID = 'ASSAD' 
-# 		OR StartAirportID = 'BEKOL' 
-# 		OR StartAirportID = 'BISUN' 
-# 		OR StartAirportID = 'BUNTA' 
-# 		OR StartAirportID = 'GOLOT' 
-# 		OR StartAirportID = 'GOPTO' 
-# 		OR StartAirportID = 'INTIK' 
-# 		OR StartAirportID = 'KAMUD' 
-# 		OR StartAirportID = 'KATBO' 
-# 		OR StartAirportID = 'LAMEN' 
-# 		OR StartAirportID = 'LANDA'
-# 		OR StartAirportID = 'LINSO' 
-# 		OR StartAirportID = 'MAGIT' 
-# 		OR StartAirportID = 'MAGOG' 
-# 		OR StartAirportID = 'MORIT' 
-# 		OR StartAirportID = 'NIXAL' 
-# 		OR StartAirportID = 'NONIM' 
-# 		OR StartAirportID = 'POLHO' 
-# 		OR StartAirportID = 'PURPA' 
-# 		OR StartAirportID = 'RCBS' 
-# 		OR StartAirportID = 'RCFN'
-# 		OR StartAirportID = 'RCQC'
-# 		OR StartAirportID = 'RCKH' 
-# 		OR StartAirportID = 'RCMQ' 
-# 		OR StartAirportID = 'RCNN' 
-# 		OR StartAirportID = 'RCSS' 
-# 		OR StartAirportID = 'RCTP' 
-# 		OR StartAirportID = 'RCYU' 
-# 		OR StartAirportID = 'RULAD' 
-# 		OR StartAirportID = 'SAGAG' 
-# 		OR StartAirportID = 'SARIN' 
-# 		OR StartAirportID = 'SARUL' 
-# 		OR StartAirportID = 'SIKOU' 
-# 		OR StartAirportID = 'SIMLI' 
-# 		OR StartAirportID = 'TAMOT'
-# 		OR StartAirportID = 'TEBAK' 
-# 		OR StartAirportID = 'TEBUS' 
-# 		OR StartAirportID = 'TELOK' 
-# 		OR StartAirportID = 'TOMUK' 
-# 		OR StartAirportID = 'VHHH' 
-# 		OR StartAirportID = 'VMMC' 
-# 		OR StartAirportID = 'ZAO'
-# 	)
-# ''')
-#endregion
-
-#region新方法
-cursor.execute=('''
+# 执行查询
+cursor.execute('''
 SELECT
     name,
-    ROUND(MinSafeAltitude * 3.281) AS 'MinSafeAltitude',
+    ROUND(MinSafeAltitude * 3.281,0) AS 'MinSafeAltitude',
     "RESTRICT",
     TRANS_ALT
 FROM
     FLIGHT_AIRLINE
 WHERE
     LENGTH(StartAirportID) = 4
-    AND not (StartAirportID LIKE 'RC%' OR StartAirportID LIKE 'VH%' or StartAirportID LIKE 'VM%' OR StartAirportID  = 'ZAO');
+    AND NOT (StartAirportID LIKE 'RC%' OR StartAirportID LIKE 'VH%' OR StartAirportID = 'VMMC' OR StartAirportID LIKE 'ZAO%');
 ''')
-RTEdata = cursor.fetchall()
-conn.close
+# 处理查询结果并进行正则表达式替换
+RTEData = []
+for row in cursor.fetchall():
+    name, min_safe_altitude, restrict, trans_alt = row
+    # 使用正则表达式进行替换
+    trans_alt = re.sub(r'(\d{2})(?=/|$)', r'S\1', trans_alt)
+    RTEData.append((name, min_safe_altitude, restrict, trans_alt))
+# 关闭数据库连接
+conn.close()
 #endregion
 
 #region 导出数据
 RTENewdata = ''
-for i in RTEdata:
+for i in RTEData:
     RTENewdata += f",,{i[0]},,{i[3]},{i[1]},,{i[2]}\n"
 print(RTENewdata)
 #endregion
