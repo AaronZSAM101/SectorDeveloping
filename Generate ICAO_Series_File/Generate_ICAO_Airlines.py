a5ae7361ddc06e4e2e974a761877f92eccf300b8
Aaron_ZSAM <CaptainAaron101@163.com>
2023-10-02 14:36:27 +0800

    修复bug：删除多余的OMA和CXA

--- Generate ICAO_Series_File/Generate_ICAO_Airlines.py
+++ Generate ICAO_Series_File/Generate_ICAO_Airlines.py
@@ -1,3 +1,5 @@
+import time
+AIRAC_CYCLE = input('当前周期号:')
 #region 删除带分号的行
 with open('ICAO_Airlines.txt', 'r', encoding='utf-8', errors='ignore') as f:
     lines = f.readlines()
@@ -9,9 +11,7 @@ with open('ICAO_Airlines.txt', 'w', encoding='utf-8', errors='ignore') as f:
 #region 删除CHINA航司
 with open("ICAO_Airlines.txt", "r", encoding='utf-8', errors='ignore') as file:
     lines = file.readlines()
-
 new_lines = []
-
 for line in lines:
     if "CHINA" in line:
         parts = line.split("\t", 3) 
@@ -20,17 +20,35 @@ for line in lines:
             new_lines.append(new_line)
     else:
         new_lines.append(line)
-
 with open("ICAO_Airlines.txt", "w", encoding='utf-8', errors='ignore') as file:
     file.writelines(new_lines)
+
+with open("ICAO_Airlines.txt", "r", encoding='utf-8', errors='ignore') as file:
+    lines = file.readlines()
+new_lines = []
+for line in lines:
+    if not line.startswith(('CXA', 'OMA')):
+        new_lines.append(line)
+with open("ICAO_Airlines.txt", "w", encoding='utf-8', errors='ignore') as file:
+    file.writelines(new_lines)
+
 #endregion
 
 #region 写入自主维护的CHINA航司
 with open('China_Callsign.txt', 'r', encoding='utf-8', errors='ignore') as f:
     CHINA_CALLSIGN = f.readlines()
-with open('ICAO_Airlines.txt', 'a', encoding='gbk', errors='ignore') as f:
+with open('ICAO_Airlines.txt', 'a+', encoding='gbk', errors='ignore') as f:
     f.writelines(CHINA_CALLSIGN)
 #endregion
 
+#region 写入注释信息
+with open('ICAO_Airlines.txt', 'r', encoding='gbk', errors='ignore') as file:
+    # 读取原始内容
+    existing_content = file.read()
+LOCAL_TIME=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+new_content = f";DATA provided by Aero-Nav & CAAC\n;CYCLE: {AIRAC_CYCLE}\n;Generate Date: {LOCAL_TIME}\n" + existing_content
+with open('ICAO_Airlines.txt', 'w', encoding='gbk', errors='ignore') as file:
+    file.write(new_content)
+#endregion
 
 print('中国呼号生成完毕')
