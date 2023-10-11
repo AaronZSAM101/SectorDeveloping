9627a049ce762637a93958edbeb4c4f24435c09f
Aaron_ZSAM <CaptainAaron101@163.com>
2023-10-12 02:38:19 +0800

    添加注释

--- Model/PRFFactory/写入指定文本.py
+++ Model/PRFFactory/写入指定文本.py
@@ -3,18 +3,18 @@ import os
 folder_path = input('Please enter the root of your sector: ')
 
 text_to_append = '''
-Settings	alias	\All\Settings\Alias.txt
-'''
 
+'''
+#上方引号中输入需要添加的内容
 # 获取文件夹内所有以.prf为后缀名的文件
-file_list = [file for file in os.listdir(folder_path) if file.endswith('_TWR.prf')]
+file_list = [file for file in os.listdir(folder_path) if file.endswith('.prf')] #修改后缀名可实现各种类型文件的操作，修改startswith或endwith可实现以某种文件名开头的文件的处理
 
 for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
     
     try:
         # 在文件末尾追加文本内容
-        with open(file_path, 'a') as file:
+        with open(file_path, 'a', errors='ignore') as file: #默认在文本末尾添加内容
             file.write(text_to_append)
         
         print(f"Appended content to file: {file_name}")
