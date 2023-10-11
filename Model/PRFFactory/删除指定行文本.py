05c9a3c5b135d658a7d90fa70d65f0fe40059404
Aaron_ZSAM <CaptainAaron101@163.com>
2023-10-12 02:36:24 +0800

    添加注释

--- Model/PRFFactory/删除指定行文本.py
+++ Model/PRFFactory/删除指定行文本.py
@@ -11,14 +11,14 @@ for file_name in file_list:
     file_path = os.path.join(folder_path, file_name)
     
     # 打开文件并读取内容
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', errors='ignore') as file:
         lines = file.readlines()
     
     # 删除第5至第56行内容
-    del lines[31:51]  # 注意：这里索引从0开始
+    del lines[31:51]  # 注意：这里索引从0开始，即：第一行为0，第二行为1，以此类推
     
     # 再次打开文件并写入修改后的内容
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', errors='ignore') as file:
         file.writelines(lines)
 
 print("删除操作已完成。")
