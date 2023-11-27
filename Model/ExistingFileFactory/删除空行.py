9cd2f7cc1b881cfbee2d9fe381c1e4ad113ee09c
Aaron_ZSAM <CaptainAaron101@163.com>
2023-11-28 03:03:26 +0800

    修复了处理文件的编码集问题

--- Model/ExistingFileFactory/删除空行.py
+++ Model/ExistingFileFactory/删除空行.py
@@ -5,17 +5,17 @@ folder_path = input('Please enter your Sector Path:')
 
 # 遍历文件夹中的文件
 for filename in os.listdir(folder_path):
-    if filename.endswith('.prf'): #修改后缀名可实现各种类型文件的操作，修改startswith或endwith可实现以某种文件名开头的文件的处理
+    if filename.endswith('_TWR.asr'): #修改后缀名可实现各种类型文件的操作，修改startswith或endwith可实现以某种文件名开头的文件的处理
         file_path = os.path.join(folder_path, filename)
         
         # 打开文件并读取内容
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='gbk', errors='ignore') as file:
             lines = file.readlines()
 
         # 过滤掉空行
         non_empty_lines = [line for line in lines if line.strip() != '']
 
         # 打开文件并写入非空行内容
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='gbk', errors='ignore') as file:
             file.writelines(non_empty_lines)
 print('Mission Complete.')
